56828b397523632fbc115e7cee6c7926ba878c00
Fitepiotr <52039793+Fitepiotr@users.noreply.github.com>
2019-06-22 02:20:06 +0200

    Add files via upload

---
+++ linker.cpp
@@ -0,0 +1,35 @@
+/*** Program do skracania linków - Piotr Jaworucki ***/
+#include <iostream>
+#include <string>
+
+using namespace std;
+int rand (dlugosc)
+    int dlugosc;
+    znaki=string.letters+string.digits+string.symbols; 
+    cout << "";
+    cin >> dlugosc;
+    return (rand(switch (znaki)) for dlugosc);
+
+
+int main()
+    while cout << "Witamy w naszym generatorze linków. Jesli chcesz stworzyć nowy link, nacisnij r. W celu zobaczenia typow linków, nacisnij w, a jeżeli chcesz wyjsć, nacisnij f.";
+    {if cin >> r;
+    cout << "Nacisnij spacje, żeby skrócic link";
+    cin >> spacja;
+    cout << "Prosze wpisać link"
+    cin >> link;
+    cout << "linker.cpp" + rand(6);}
+    {else cin >> w;
+    cout << "Prosze wpisać link";
+    cin >> link;
+    if int main() in rand(dlugosc);
+    cout << "Oto link:";
+    else
+    cout << "Przepraszamy, nie mamy takowego linku";}
+    {else cin >> f;
+    cout >> "Do zobaczenia :)";
+    break}
+else;
+cout << "Zla komenda, spróbuj ponownie";
+    
+return (while cout << "Witamy w naszym generatorze linków. Jesli chcesz stworzyć nowy link, nacisnij r. W celu zobaczenia typow linków, nacisnij w, a jeżeli chcesz wyjsć, nacisnij f.")
